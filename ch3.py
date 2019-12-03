30d8e990d800516aae6554ec4c2405ba0c184c38
Chris McBride <christopher.mcbride@gmail.com>
2019-12-03 04:40:36 +0000

    partially working p-controller. jerky motion

--- ch3.py
+++ ch3.py
@@ -4,6 +4,9 @@ from di_sensors.easy_distance_sensor import EasyDistanceSensor
 from time import sleep
 
 class RudolphBot:
+	TAU = 0.4
+	OBS_SAFE_DIST = 6
+	
 	def __init__(self):
 		self.gpg = EasyGoPiGo3()
 		self.sensor = EasyIMUSensor(port='AD2')
@@ -11,20 +14,71 @@ class RudolphBot:
 		self.distance_sensor = self.gpg.init_distance_sensor("GPG3_AD1")
 		
 		print("current heading: %f" % self.get_current_heading())
+		print("target heading: %f" % self.target_heading) 
 
 	def get_current_heading(self):
-		heading, _, _ = self.sensor.safe_read_euler()
+		heading, _, _ = self.sensor.safe_read_euler() 
 		return heading
-		
-	def visit_santa(self):
+	
+	def check_for_obstacles(self):
 		obstacle_distance = self.distance_sensor.read_inches()
-		print("Nearest Obstacle: %f" % obstacle_distance)
 		
-		if obstacle_distance < 6:
+		if obstacle_distance < RudolphBot.OBS_SAFE_DIST:
 			print("I'll never see Santa with all these roadblocks!")
+			self.gpg.stop()
+			return True
+
+		print("I'm on my way Comet, don't eat my pancakes!")
+		return False
+
+	def visit_santa(self):
+		while True:
+			if self.check_for_obstacles():
+				break
+				
+			self.gpg.forward() # (5)
+			#sleep(0.5)
+			#sleep(1.0)
+			
+			alpha = self.p_controller_get_angle()
+			#if alpha > 15 or alpha < -15:
+			#	alpha = 15
+			if abs(alpha) < 1:
+				break
+				
+			#self.gpg.turn_degrees(alpha)
+			if abs(alpha) > 0:
+				left()
+			else:
+				right()
+			
+		self.stop()
+		
+	def p_controller_get_angle(self):
+		current = self.get_current_heading()
+		#print('current heading is: %f' % current)
+		
+		if current >= 180:
+			cte = 360 - current
 		else:
-			print("I'm on my way Comet, don't eat my pancakes!")
+			cte = -current
+		
+		#cte = self.sensor.safe_north_point()
+		
+		print('current cte is: %f' % cte)
+		
+		alpha = RudolphBot.TAU * cte
+		print('p-controller alpha value is: %f' % alpha)
+		
+		return alpha
+		
+	def stop(self):
+		self.gpg.stop()
 		
 if __name__ == "__main__":
 	nb = RudolphBot()
 	nb.visit_santa()
+	nb.stop()
+	
+	
+		
